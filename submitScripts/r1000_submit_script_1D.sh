5435cf2ea9a319547c0577966f201a793ca1cc61
Theo Dorfner <theo.dorfner@student.tuwien.ac.at>
2023-03-30 20:29:04 +0200

    absolutely last changes - I'll upload now

--- submitScripts/r1000_submit_script_1D.sh
+++ submitScripts/r1000_submit_script_1D.sh
@@ -10,26 +10,17 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=40
 #SBATCH --cpus-per-task=1
-#SBATCH --time=01:30:00 #set time to 1h30min of intial tests
+#SBATCH --time=00:30:00
 
 ## load modules
 
 module load pmi/pmix-x86_64
 module load mpi/openmpi-x86_64
 
-## start compilation (?should we compile before that? i.e. manually to catch errors)
+## start compilation
 mpic++ -std=c++17 -O3 -Wall -pedantic -march=native -ffast-math ../src/jacobiMPI.cpp -o jacobiMPI
 
-
-## submitting jobs
-
-## I don't know why they print the hostname in the original job submission, but yeah
-
-##srun hostname
-
 ## now this is the proper job
-## ./jacobiMPI <resolution> <iterations>
-
 for n in {1,2,3,5,7,10,12,15,17,20,22,25,27,30,32,35,37,40}
 do
     srun --mpi=pmix --ntasks=${n} ./jacobiMPI 1D 1000 30
